66809104f294d77262a10bac2eae6c89a12ddb4e
Pleroy33 <145795478+Pleroy33@users.noreply.github.com>
2024-03-21 14:11:27 -0500

    Initialize featureEditDogProfile

---
+++ server/server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const PORT = process.env.PORT || 5001;
+
+// Middleware Includes
+const sessionMiddleware = require('./modules/session-middleware');
+const passport = require('./strategies/user.strategy');
+
+// Route Includes
+const userRouter = require('./routes/user.router');
+
+const dogRouter = require('./routes/dog.router')
+const raiserRouter = require('./routes/raiserDog.router')
+
+
+const sitterRouter = require('./routes/sitter.router');
+
+const mailRouter = require('./routes/mail.router');
+
+
+// Express Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('build'));
+
+// Passport Session Configuration
+app.use(sessionMiddleware);
+
+// Start Passport Sessions
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Routes
+app.use('/api/user', userRouter);
+app.use('/api/dog', dogRouter);
+
+app.use('/api/raiser-dog', raiserRouter);
+
+app.use('/api/sitter', sitterRouter);
+
+// app.use('/api/mail', mailRouter);
+
+
+// Listen Server & Port
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
---
+++ src/components/App/App.jsx
@@ -0,0 +1,132 @@
+import React, { useEffect } from "react";
+import {
+  HashRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import theme from "../../theme";
+// import components
+import AboutPage from "../AboutPage/AboutPage";
+import AddDogForm from "../AddDogForm/AddDogForm";
+import AdminHome from "../AdminHome/AdminHome";
+import DataGrid from "../DataGrid/DataGrid";
+import DogCards from "../AllDogForm/AllDogForm";
+import DogProfile from "../DogProfile/DogProfile";
+import EditDogProfile from "../EditDogProfile/EditDogProfile";
+import Footer from "../Footer/Footer";
+import InfoPage from "../InfoPage/InfoPage";
+import LandingPage from "../LandingPage/LandingPage";
+import LoginPage from "../LoginPage/LoginPage";
+import Nav from "../Nav/Nav";
+import ProtectedRoute from "../ProtectedRoute/ProtectedRoute";
+import RaiserDogPage from "../RaiserDogPage/RaiserDogPage";
+import RegisterPage from "../RegisterPage/RegisterPage";
+import RequestCareForm from "../RequestCareForm/RequestCareForm";
+import SitterHomePage from "../SitterHome/SitterHome";
+import UserPage from "../UserPage/UserPage";
+import UserProfileEdit from "../UserProfileEdit/UserProfileEdit";
+import VolunteerSitterForm from "../VolunteerSitterForm/VolunteerSitterForm";
+import "./App.css";
+function App() {
+  const dispatch = useDispatch();
+  const user = useSelector((store) => store.user);
+  const customTheme = createTheme(theme);
+
+  useEffect(() => {
+    dispatch({ type: "FETCH_USER" });
+  }, [dispatch]);
+
+  return (
+    <ThemeProvider theme={customTheme}>
+      <Router>
+        <div>
+          <Nav />
+          <Switch>
+            <Redirect exact from="/" to="/home" />
+            <Route exact path="/about">
+              <AboutPage />
+            </Route>
+            {/* Add the new ProtectedRoutes here */}
+
+            <ProtectedRoute exact path="/sitter-home">
+              <SitterHomePage />
+            </ProtectedRoute>
+            <ProtectedRoute exact path="/admin-home">
+              <AdminHome />
+            </ProtectedRoute>
+            <ProtectedRoute exact path="/add-dog-form">
+              <AddDogForm />
+            </ProtectedRoute>
+            <ProtectedRoute exact path="/data-grid">
+              <DataGrid />
+            </ProtectedRoute>
+            <ProtectedRoute exact path="/raiser-home">
+              <RaiserDogPage />
+            </ProtectedRoute>
+            <ProtectedRoute exact path="/user-edit">
+              <UserProfileEdit />{" "}
+            </ProtectedRoute>
+            <ProtectedRoute exact path="/user">
+              <UserPage />
+            </ProtectedRoute>
+            <ProtectedRoute exact path="/info">
+              <InfoPage />
+            </ProtectedRoute>
+            <ProtectedRoute exact path="/home">
+              <RaiserDogPage />
+            </ProtectedRoute>
+            <ProtectedRoute exact path="/sitterHome">
+              <SitterHomePage />
+            </ProtectedRoute>
+            <ProtectedRoute exact path="/adminHome">
+              <AdminHome />
+            </ProtectedRoute>
+            <ProtectedRoute exact path="/allDogCards">
+              <DogCards />
+            </ProtectedRoute>
+            <ProtectedRoute exact path="/dataGrid">
+              <DataGrid />
+            </ProtectedRoute>
+            <ProtectedRoute exact path="/userEdit">
+              <UserProfileEdit />
+            </ProtectedRoute>
+            <ProtectedRoute exact path="/user">
+              <UserPage />
+            </ProtectedRoute>
+            <ProtectedRoute exact path="/info">
+              <InfoPage />
+            </ProtectedRoute>
+            <ProtectedRoute exact path="/volunteersitterform">
+              <VolunteerSitterForm />
+            </ProtectedRoute>
+            <ProtectedRoute exact path="requestcareform/:dogId"></ProtectedRoute>
+            {/* //! right now the request care form is not working, UNLESS it is pulling the dog id */}
+  
+
+            <Route exact path="/login">
+              {user.id ? <Redirect to="/user" /> : <LoginPage />}
+            </Route>
+            <Route exact path="/registration">
+              {user.id ? <Redirect to="/user" /> : <RegisterPage />}
+            </Route>
+            <Route exact path="/home">
+              {user.id ? <Redirect to="/user" /> : <LandingPage />}
+            </Route>
+            <Route path="/dogProfile/:dogId" component={DogProfile} />
+            <Route path="/editdogprofile/:dogId" component={EditDogProfile} />
+            <Route path="/requestcareform/dogId" component={RequestCareForm} />
+            <Route>
+              <h1>404 - Page Not Found</h1>
+            </Route>
+          </Switch>
+          <Footer />
+        </div>
+      </Router>
+    </ThemeProvider>
+  );
+}
+
+export default App;
